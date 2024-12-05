65e7992eb9fcf9ca834a1d44423ad2e0a39059e5
mzrt <vdksemaluv@icloud.com>
2024-12-05 15:52:51 +0300

    Добавьте файлы проекта.

---
+++ Hakaton/DataEntry.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hakaton
+{
+    class DataEntry
+    { 
+        public long TimeUsec { get; set; } // Время в микросекундах
+        public double EciQuatW { get; set; } // Кватернион ECI.w
+        public double EciQuatX { get; set; } // Кватернион ECI.x
+        public double EciQuatY { get; set; } // Кватернион ECI.y
+        public double EciQuatZ { get; set; } // Кватернион ECI.z
+        public double OrbQuatW { get; set; } // Кватернион Orb.w
+        public double OrbQuatX { get; set; } // Кватернион Orb.x
+        public double OrbQuatY { get; set; } // Кватернион Orb.y
+        public double OrbQuatZ { get; set; } // Кватернион Orb.z
+        public double Latitude { get; set; } // Широта в градусах
+        public double Longitude { get; set; } // Долгота в градусах
+        public double Altitude { get; set; } // Высота в метрах
+    }
+}
---
+++ Hakaton/Program.cs
@@ -0,0 +1,81 @@
+﻿using Hakaton;
+using System.Text.RegularExpressions;
+
+string rootPath = @"C:\Users\kvale\OneDrive\Рабочий стол\САФУ хакатон\САФУ хакатон\Photo_telemetry_hackaton";
+string[] imaginePaths = [];
+string logs = "";
+string logFile = "";
+string photo = "";
+if (Directory.Exists(rootPath))
+{
+    string[] directoryEntries = Directory.GetDirectories(rootPath);
+    for (int i = 0; i < directoryEntries.Length; i++)
+    {
+        imaginePaths.Append(Directory.GetFiles(directoryEntries[i], "*.jpg", SearchOption.AllDirectories)[0]);
+        string dateTimePattern = @"(\d{4})-(\d{2})-(\d{2})_(\d{2})-(\d{2})-(\d{2})";
+
+        Match match = Regex.Match(imaginePaths[0], dateTimePattern);
+
+        if (match.Success)
+        {
+            int year = int.Parse(match.Groups[1].Value);
+            int month = int.Parse(match.Groups[2].Value);
+            int day = int.Parse(match.Groups[3].Value);
+            int hour = int.Parse(match.Groups[4].Value);
+            int minute = int.Parse(match.Groups[5].Value);
+            int second = int.Parse(match.Groups[6].Value);
+
+            DateTime dateTime = new DateTime(year, month, day, hour, minute, second);
+        }
+        logs = Directory.GetDirectories(rootPath)[0];
+        logFile = Directory.GetFiles(logs)[0];
+        var beacons = new List<DataEntry>();
+
+        if (File.Exists(logFile))
+        {
+            using (StreamReader reader = new StreamReader(logFile))
+            {
+                int lineNumber = 0;
+                bool skipFirstTwoLines = true;
+
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    lineNumber++;
+
+                    if (skipFirstTwoLines)
+                    {
+                        if (lineNumber > 2)
+                        {
+                            skipFirstTwoLines = false;
+                        }
+                        continue;
+                    }
+                    if (line != string.Empty)
+                    {
+                        string[] fields = line.Split(',');
+                        DataEntry data = new()
+                        {
+                            TimeUsec = Convert.ToInt32(fields[0]),
+                            EciQuatW = Convert.ToDouble(fields[1]),
+                            EciQuatX = Convert.ToDouble(fields[2]),
+                            EciQuatY = Convert.ToDouble(fields[3]),
+                            EciQuatZ = Convert.ToDouble(fields[4]),
+                            Latitude = Convert.ToDouble(fields[5]),
+                            Longitude = Convert.ToDouble(fields[6]),
+                            Altitude = Convert.ToDouble(fields[7])
+                        };
+                        beacons.Add(data);
+                    }
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Файл не найден.");
+        }
+        string kmlFilePath = @"C:\Users\kvale\OneDrive\Рабочий стол\САФУ хакатон\САФУ хакатон\Kmls";
+        GenerateKmlFile(beacons, kmlFilePath);
+    }
+}
+    
